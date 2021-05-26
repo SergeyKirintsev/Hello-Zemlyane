683700d0f774d7f878ebae28a282bb2020f0fdde
SergeyKirintsev <kserg80@gmail.com>
2021-05-27 01:04:45 +0300

    Сделать обработку формы

--- app.js
+++ app.js
@@ -3,26 +3,17 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-// parse application/json
 app.use(bodyParser.json());
-
-// app.use("/", function (request, response) {
-//   response.send("<h1>Главная страница</h1>");
-// });
 
 app.use(express.static(__dirname + "/public"));
 
 app.post("/about", async function (req, res) {
-  // console.log("about:", req.query);
-  console.log("about:", req.body);
+  const { username, age } = req.body
+  console.log("username:", username);
+  console.log("age:", age);
   // res.sendStatus(200);
-  res.status(404).send(`Ресурс не найден`);
-  // res.send("about");
-});
-
-app.post("/create", function (req, res) {
-  console.log("create:", req.query);
-  res.send("create");
+  // res.status(404).send(`Ресурс не найден`);
+  res.send("about");
 });
 
 app.listen(8080, () => console.log("App is starteed..."));
--- public/main.js
+++ public/main.js
@@ -7,16 +7,25 @@ aboutBtn.addEventListener("click", (evt) => {
 });
 
 function createCard() {
+  const data = getInputValues();
   return fetch(`http://localhost:8080/about`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      name: "Sergey",
-      link: "Kirin",
-    }),
+    body: JSON.stringify(data),
   })
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
+}
+
+function getInputValues() {
+  const form = document.querySelector('form')
+  const inputList = form.querySelectorAll('input')
+
+  const inputValues = {};
+  inputList.forEach((input) => {
+    inputValues[input.name] = input.value;
+  });
+  return inputValues;
 }
